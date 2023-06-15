ff324d0242ef6ecde6faa44c09e41a6fe23ffeed
realtimshady1 <timothy.guo3@gmail.com>
2023-06-15 22:16:35 +1000

    fixed bug for opening image in opencv

--- opencv/nuclio/model_handler.py
+++ opencv/nuclio/model_handler.py
@@ -3,35 +3,38 @@
 # SPDX-License-Identifier: MIT
 
 import cv2
-from copy import copy
 import numpy as np
+
+from copy import copy
+from PIL import Image
 
 class ModelHandler:
     def __init__(self):
         self.tracker = cv2.legacy.TrackerMedianFlow_create()
 
     def infer(self, image, shape, state):
-        image = np.array(image)
-        state = np.array(state)
-        
-        x1, y1, x2, y2 = shape
-        w = x1 - x2
-        h = y1 - y2
-        bbox = [x1, y1, w, h]
-        
         # init tracking
         if state is None: 
             pass
             
         # track
         else:
+            prev_image = Image.open(state)
+            state = np.array(state)
+            prev_image = np.array(prev_image)
+            
+            x1, y1, x2, y2 = shape
+            w = x1 - x2
+            h = y1 - y2
+            bbox = [x1, y1, w, h]
             tracker = copy(self.tracker)
-            ret = tracker.init(state, bbox)
+            
+            ret = tracker.init(prev_image, bbox)
             ret, bbox = tracker.update(image)
             
-        x1, y1, w, h = bbox
-        x2 = x1 + w
-        y2 = y1 + h
-        shape = [x1, y1, x2, y2]
+            x1, y1, w, h = bbox
+            x2 = x1 + w
+            y2 = y1 + h
+            shape = [x1, y1, x2, y2]
 
         return shape
